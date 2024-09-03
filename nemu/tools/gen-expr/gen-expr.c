a1e5e1f0828a75cd67069f716af14318a95a2343
tracer-ysyx <tracer@ysyx.org>
2024-09-03 15:11:43 +0800

    > compile NEMU ysyx_22040000 张三 Linux dongtaiheng-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:11:43 up 12:53,  1 user,  load average: 0.81, 2.35, 3.24

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -1,116 +1,133 @@
-#include <signal.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <assert.h>
 #include <string.h>
+#include <math.h>
 
 // this should be enough
 static char buf[65536] = {};
-//buf[65535]='\0';
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
-"#include <signal.h>\n"
-"#include <string.h>\n"
-"int main() { \n"
-"  unsigned result = %s; \n"
-"  printf(\"%%u \", result); \n"
-"  return 0; \n"
-"}\n";
+"int main() { "
+"  unsigned result = %s; "
+"  printf(\"%%u\", result); "
+"  return 0; "
+"}";
 
+char *buf_ptr = buf;
+int char_num;
 
- void gen_rand_op();
- void gen(char c);
- void gen_num();
-uint32_t choose(uint32_t n);
-
- void gen_rand_expr() {
-
-	switch(choose(3)){
-		case 0:gen_num();break;
-		case 1:gen('(');gen_rand_expr();gen(')');break;
-		default:gen_rand_expr();gen_rand_op();gen_rand_expr();break;
-	} 
-	
+//generate a random value
+uint32_t choose(uint32_t n)
+{
+    int lower = 0;
+    int upper = n;
+    uint32_t randomInRange = (abs(rand()) % (upper - lower)) + lower;
+    return randomInRange;
 }
 
-uint32_t choose(uint32_t n){
-	return (rand()% n);
+int getDigitCount(uint32_t number) {
+    number = abs(number); 
+    if (number == 0) {
+        return 1;
+    }
+    return (int)log10(number) + 1;
+}
+
+void gen_num()
+{
+  int lower = 1;
+  int upper = 100;
+  // uint32_t randomNumber = rand();
+  uint32_t randomNumber = (abs(rand()) % (upper - lower)+1) + lower;
+  sprintf(buf_ptr, "%d", randomNumber);
+  if(randomNumber < 0)
+    buf_ptr +=getDigitCount(randomNumber+1);
+  else
+    buf_ptr +=getDigitCount(randomNumber);
+  char_num+=randomNumber;
+}
+
+void gen(const char c)
+{
+  *(buf_ptr++) = c;
+    char_num+=1;
 }
 
 void gen_rand_op()
 {
-	char op;
-	switch(choose(4)){
-		case 0:op='+';break;
-		case 1:op='-';break;
-		case 2:op='*';break;
-		default:op='/';break;
-	}
-	gen(op);
+  switch (choose(4)) {
+    case 0: *(buf_ptr++) = '+';break;
+    case 1: *(buf_ptr++) = '-';break;
+    case 2: *(buf_ptr++) = '*';break;
+    case 3: *(buf_ptr++) = '/';break;
+  }
+  char_num+=1;
 }
 
- void gen(char c)
+void gen_rand_expr() 
 {
-	if(buf[65535]!='\0') return;
-	int j=0;
-	j=strlen(buf);
-	buf[j]=c;
-	if(j+1<65536) buf[j+1]='\0';
+  if(char_num < 65536)
+  {
+    switch (choose(4)) {
+      case 0: gen_num();break;
+      case 1: gen('(');  gen_rand_expr(); gen(')'); break;
+      case 2: gen(' ');  gen_rand_expr(); gen(' '); break;
+      default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+    }
+  }
 }
- void  gen_num(){
-	if(buf[65535]!='\0') return;
-	 
-	uint32_t generated_num=0;
-	char str[10];int j=0,p;
-	generated_num=choose(1000);
-	sprintf(str,"%u",generated_num);
-	j=strlen(buf);
-	/*while(buf[j]!='\0'&&j<65535)
-	{j++;}*/
-	for(p=0;p<strlen(str)&&j<65536;p++)
-	{
-		buf[j++]=str[p];
-	}
-	if(j<65536)	buf[j]='\0';
-}
-
-
 
 int main(int argc, char *argv[]) {
   int seed = time(0);
   srand(seed);
   int loop = 1;
-  if (argc > 1) { 
+  if (argc > 1) {
     sscanf(argv[1], "%d", &loop);
   }
   int i;
   for (i = 0; i < loop; i ++) {
-		buf[0] = '\0';
-		buf[65535] = '\0';
+    memset(buf,'\0', sizeof(buf));
+    buf_ptr = buf;
+    char_num = 0;
     gen_rand_expr();
-		if(buf[0]=='\0')	return 0; 
-		else 
-			sprintf(code_buf, code_format, buf);
+    if(char_num > 65536)
+    {
+      // printf("\033[42mcore dump\033[0m\n");
+      continue;
+    }
+    sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
     assert(fp != NULL);
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc -Werror=div-by-zero  -Werror=overflow /tmp/.code.c -o /tmp/.expr");
-    if (ret != 0) {i--;continue;}
-
-    fp = popen("/tmp/.expr", "r");
-		
+    fp = popen("gcc /tmp/.code.c -o /tmp/.expr 2>&1" , "r");
     assert(fp != NULL);
 
-		int result;
-     ret = fscanf(fp, "%u",&result);
-     printf("%u %s\n", result, buf);
+    char buffer[128];
+    if (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        // printf("phb%s", buffer);
+        pclose(fp);
+        // printf("\033[41moverflow/div_zero\033[0m\n");
+        continue;
+    }
     pclose(fp);
-  } 
+
+    fp = popen("/tmp/.expr", "r");
+    assert(fp != NULL);
+
+    int result;
+
+    int ret = fscanf(fp, "%d", &result);
+    if (ret == 0) continue;
+    pclose(fp);
+
+    printf("%u %s\n", result, buf);
+  }
   return 0;
 }
