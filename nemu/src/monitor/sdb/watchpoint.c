05f08d7523501645596b2642a81520bfbede236b
tracer-ysyx <tracer@ysyx.org>
2024-09-12 18:04:59 +0800

    > compile NEMU ysyx_22040000 张三 Linux dongtaiheng-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:04:59 up 24 min,  1 user,  load average: 0.37, 0.23, 0.38

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -13,7 +13,6 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 #include "sdb.h"
-
 #define NR_WP 32
 #include "watchpoint.h"
 
