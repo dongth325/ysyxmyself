e26ab83dc954321885b116d8eb2487fee207258f
tracer-ysyx <tracer@ysyx.org>
2024-09-23 19:21:04 +0800

    > compile NEMU ysyx_22040000 张三 Linux dongtaiheng-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:21:04 up 1 day,  2:12,  1 user,  load average: 0.29, 0.25, 0.40

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -8,15 +8,23 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S,
+  TYPE_I, TYPE_U, TYPE_S, TYPE_J,
   TYPE_N, // none
 };
 
 #define src1R() do { *src1 = R(rs1); } while (0)
 #define src2R() do { *src2 = R(rs2); } while (0)
-#define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
-#define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
-#define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define immI()  do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
+#define immU()  do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
+#define immS()  do { *imm = SEXT((BITS(i, 31, 25) << 5) | BITS(i, 11, 7), 12); } while(0)
+#define immJ()  do { \
+  uint32_t imm20 = BITS(i,31,31); \
+  uint32_t imm10_1 = BITS(i,30,21); \
+  uint32_t imm11 = BITS(i,20,20); \
+  uint32_t imm19_12 = BITS(i,19,12); \
+  uint32_t imm_ = (imm20 << 20) | (imm19_12 << 12) | (imm11 << 11) | (imm10_1 << 1); \
+  *imm = SEXT(imm_,21); \
+} while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -27,6 +35,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
+    case TYPE_J:                   immJ(); break;
   }
 }
 
@@ -42,9 +51,16 @@ static int decode_exec(Decode *s) {
 }
 
   INSTPAT_START();
+  // 已有的指令
   INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(rd) = imm);
   INSTPAT("??????? ????? ????? 010 ????? 00000 11", lw     , I, R(rd) = Mr(src1 + imm, 4));
   INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S, Mw(src1 + imm, 4, src2));
+
+  // 新添加的指令
+  INSTPAT("??????? ????? ????? 000 ????? 0010011", addi   , I, R(rd) = src1 + imm);
+  INSTPAT("??????? ????? ????? ??? ????? 0010111", auipc  , U, R(rd) = s->pc + imm);
+  INSTPAT("??????? ????? ????? ????? ????? 1101111", jal  , J, { R(rd) = s->pc + 4; s->dnpc = s->pc + imm; });
+  INSTPAT("??????? ????? ????? 000 ????? 1100111", jalr   , I, { word_t t = s->pc + 4; s->dnpc = (src1 + imm) & ~1; R(rd) = t; });
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
