0ce804a14397d3f4e93a8a5f555f782f07e1850c
tracer-ysyx <tracer@ysyx.org>
2024-09-24 16:47:42 +0800

    > compile NEMU ysyx_22040000 张三 Linux dongtaiheng-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:47:42 up 1 day,  5:00,  1 user,  load average: 0.28, 0.27, 0.29

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -11,7 +11,7 @@ CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
-NEMUFLAGS += -l  $(shell dirname $(IMAGE).elf)/nemu-log.txt -b
+NEMUFLAGS += -l  $(shell dirname $(IMAGE).elf)/nemu-log.txt -b    #加上-b开启批处理模式
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/nemu/include
