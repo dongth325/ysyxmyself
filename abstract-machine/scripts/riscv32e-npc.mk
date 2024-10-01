2b42c098a7a02df7c004d6291e0b7d5385f92f25
tracer-ysyx <tracer@ysyx.org>
2024-10-01 16:42:29 +0800

    > compile NEMU ysyx_22040000 张三 Linux dongtaiheng-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:42:29 up 2 days, 11:57,  1 user,  load average: 0.60, 0.98, 0.68

--- abstract-machine/scripts/riscv32e-npc.mk
+++ abstract-machine/scripts/riscv32e-npc.mk
@@ -3,20 +3,6 @@ include $(AM_HOME)/scripts/platform/npc.mk
 COMMON_CFLAGS += -march=rv32e_zicsr -mabi=ilp32e  # overwrite
 LDFLAGS       += -melf32lriscv                    # overwrite
 
-
-# 如果需要定义 ISA_H
-CFLAGS  += -DISA_H=\"riscv/riscv.h\"
-
-# 确保使用正确的架构标志
-COMMON_CFLAGS += -march=rv32em_zicsr -mabi=ilp32e  # overwrite (如果 NPC 支持 rv32em_zicsr)
-# 或者
-COMMON_CFLAGS += -march=rv32e_zicsr -mabi=ilp32e  # overwrite (根据 NPC 支持情况)
-
-# 覆盖链接器标志
-LDFLAGS       += -melf32lriscv                    # overwrite
-
-
-
 AM_SRCS += riscv/npc/libgcc/div.S \
            riscv/npc/libgcc/muldi3.S \
            riscv/npc/libgcc/multi3.c \
