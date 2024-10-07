c1b2b25bd6f964844c205621e0dc4fc64f79d14f
tracer-ysyx <tracer@ysyx.org>
2024-10-07 17:20:23 +0800

    > compile NEMU ysyx_22040000 张三 Linux dongtaiheng-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:20:23 up 6 days,  8:56,  1 user,  load average: 0.54, 0.42, 0.37

--- npc/obj_dir/Vysyx_24090012_NPC.mk
+++ npc/obj_dir/Vysyx_24090012_NPC.mk
@@ -35,7 +35,7 @@ VM_PREFIX = Vysyx_24090012_NPC
 VM_MODPREFIX = Vysyx_24090012_NPC
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
-	-I/home/dongtaiheng/desktopp/ffuck/ysyx-workbench/nemu/include -I/home/dongtaiheng/desktopp/ffuck/ysyx-workbench/nemu/src/isa/riscv32/include \
+	-I/home/dongtaiheng/desktopp/ffuck/ysyx-workbench/nemu/include -I/home/dongtaiheng/desktopp/ffuck/ysyx-workbench/nemu/src/isa/riscv32/include -I/home/dongtaiheng/desktopp/ffuck/ysyx-workbench/nemu/build/generated -D__GUEST_ISA__=riscv32 \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
