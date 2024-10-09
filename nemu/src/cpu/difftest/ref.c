4e929f928ecdd2f49f8d3eb92edfc14d662367d1
tracer-ysyx <tracer@ysyx.org>
2024-10-09 19:16:01 +0800

    > compile NEMU ysyx_22040000 张三 Linux dongtaiheng-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:16:01 up 7 days, 12 min,  1 user,  load average: 1.87, 1.62, 1.43

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -23,9 +23,10 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
     // 将DUT的内存数据复制到REF
     printf("mmmmmmmmmmmmmmmmmmmmmmmmmm");//dddddddddddddddddd
     memcpy(guest_to_host(addr), buf, n);
+    printf("nnnnnnnnnnnnnnnnnnnnn");//dddddddddddddddddddd
   } else {
     // 将REF的内存数据复制到DUT
-    printf("nnnnnnnnnnnnnnnnnnnnn");//dddddddddddddddddddd
+   
     memcpy(buf, guest_to_host(addr), n);
   }
 }
@@ -36,9 +37,10 @@ __EXPORT void difftest_regcpy(void *dut, bool direction) {
         // 将DUT的寄存器状态复制到参考模型(REF)
         printf("sssssssssssssssssssssss");//ddddddddddddddddddddddd
         memcpy(dut, &cpu, sizeof(CPU_state));
+         printf("pppppppppppppppppppp");//dddddddddddddddddddddddd
     } else {
         // 将参考模型(REF)的寄存器状态复制到DUT
-        printf("pppppppppppppppppppp");//dddddddddddddddddddddddd
+       
         memcpy(&cpu, dut, sizeof(CPU_state));
     }
 
