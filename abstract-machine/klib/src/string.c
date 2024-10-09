3225d154c9bab92a73976c06a6bd0f7e2a09dc35
tracer-ysyx <tracer@ysyx.org>
2024-10-09 20:58:37 +0800

    > compile NEMU ysyx_22040000 张三 Linux dongtaiheng-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:58:37 up 7 days, 22 min,  1 user,  load average: 0.78, 0.87, 1.19

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -37,19 +37,7 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-      // 将in指向的内存区域复制到out指向的内存区域，共n个字节
-      printf("vvvvvvvvvvvvvvvvvvvvvvvvvvvv\n");//dddddddddddddddddddddddddddddd
-    unsigned char *d = (unsigned char *)out;
-    const unsigned char *s = (const unsigned char *)in;
-
-    // 遍历并逐字节复制
-    for (size_t i = 0; i < n; i++) {
-
-        d[i] = s[i];
-    }
-    printf("rrrrrrrrrrrrrrrrrrrrrrrr\n");//dddddddddddddddddddddddddddddd
-
-    return out;
+   
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
