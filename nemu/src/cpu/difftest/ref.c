630fce62e868e9563c330c9a8bc99514b2f4ea85
tracer-ysyx <tracer@ysyx.org>
2024-10-17 18:55:47 +0800

    > compile NEMU ysyx_22040000 张三 Linux dongtaiheng-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:55:47 up 1 day, 19:49,  1 user,  load average: 1.92, 1.91, 1.71

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -40,7 +40,7 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
   printf("maekmakemakemakemakemakem\n");
       if (direction == DIFFTEST_TO_DUT) {
-       // printf("zzzzzzzzzzzz\n");
+        printf("zzzzzzzzzzzz\n");
     // 将参考模型 (REF) 的寄存器状态复制到 DUT
     for (int i = 0; i < 32; i++) {
       //printf("dong dong dong %d\n",i);
