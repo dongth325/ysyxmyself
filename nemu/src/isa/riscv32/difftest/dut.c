21c9ff0e21469f30dc8c49047e4006aaa5473155
tracer-ysyx <tracer@ysyx.org>
2024-10-19 18:44:40 +0800

    > compile NEMU ysyx_22040000 张三 Linux dongtaiheng-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:44:40 up 2 days,  8:20,  1 user,  load average: 0.86, 0.32, 0.26

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,23 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+    
+  if (cpu.pc != pc) {
+   printf("PC mismatch: NEMU PC = 0x%x, REF PC = 0x%x from (bool isa_difftest_checkregs)\n", cpu.pc, pc);
+    return false;
+  }
+
+  
+  for (int i = 0; i < 32; i++) {
+    if (cpu.gpr[i] != ref_r->gpr[i]) {
+     printf("Register x%d mismatch: NEMU = 0x%x, REF = 0x%x from (bool isa_difftest_checkregs)\n",
+       i, cpu.gpr[i], ref_r->gpr[i]);
+      return false;
+    }
+  }
+
+  
+  return true;
 }
 
 void isa_difftest_attach() {
