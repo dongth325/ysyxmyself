c36608ac711aaab5ebbf05ea27fd51c34372c46e
tracer-ysyx <tracer@ysyx.org>
2024-11-03 13:47:02 +0800

    > compile NEMU ysyx_22040000 张三 Linux dongtaiheng-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:47:02 up 10:35,  1 user,  load average: 0.97, 0.37, 0.22

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -8,14 +8,62 @@ int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
 
+static int itoa(int n, char *s) {
+  int i = 0, sign = n;
+  if (sign < 0) n = -n;
+  
+  // 反向生成数字字符
+  do {
+    s[i++] = n % 10 + '0';
+  } while ((n /= 10) > 0);
+  
+  if (sign < 0) s[i++] = '-';
+  s[i] = '\0';
+
+  // 反转字符串
+  for (int j = 0; j < i / 2; j++) {
+    char temp = s[j];
+    s[j] = s[i - j - 1];
+    s[i - j - 1] = temp;
+  }
+  
+  return i;  // 返回字符串长度
+}
+
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  char *buffer = out;
+  
+  while (*fmt) {
+    if (*fmt == '%') {
+      fmt++;
+      if (*fmt == 'd') {  // 处理 %d
+        int value = va_arg(ap, int);
+        char temp[32];
+        int len = itoa(value, temp);
+        for (int i = 0; i < len; i++) *buffer++ = temp[i];
+      } 
+      else if (*fmt == 's') {  // 处理 %s
+        char *str = va_arg(ap, char*);
+        while (*str) *buffer++ = *str++;
+      }
+    } 
+    else {
+      *buffer++ = *fmt;
+    }
+    fmt++;
+  }
+  
+  *buffer = '\0';
+  return buffer - out;  // 返回写入的字符数
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int result = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return result;
 }
-
 int snprintf(char *out, size_t n, const char *fmt, ...) {
   panic("Not implemented");
 }
