ad305e6dd8e62b8c6e89bc955dbd6b30f9e47d4f
tracer-ysyx <tracer@ysyx.org>
2024-11-17 13:59:21 +0800

    > compile NEMU ysyx_22040000 张三 Linux dongtaiheng-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:59:21 up 1 day,  7:54,  1 user,  load average: 2.24, 1.17, 0.71

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -19,7 +19,7 @@ Context* __am_irq_handle(Context *c) {
     }
 
     c = user_handler(ev, c);
-      printf("ev.event4=%d",ev.event);
+      //printf("ev.event4=%d",ev.event);
     assert(c != NULL);
   }
   
