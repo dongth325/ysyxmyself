25d89c9cf55de822b94ae48f8f491dd4e0ee9381
tracer-ysyx <tracer@ysyx.org>
2025-03-06 20:16:06 +0800

    > compile NEMU ysyx_22040000 张三 Linux dongtaiheng-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:16:06 up 1 day,  8:01,  1 user,  load average: 1.52, 0.85, 0.64

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -100,7 +100,9 @@ static int parse_args(int argc, char *argv[]) {
 
 void init_monitor(int argc, char *argv[]) {
   /* Perform some global initialization. */
-
+  for(int i = 0; i < argc; i++) {
+    printf("argv[%d]: %s\n", i, argv[i]);
+  }
   /* Parse arguments. */
   parse_args(argc, argv);
 
