5fce809ef4113a675d74562646a082c2f5d25bea
tracer-ysyx <tracer@ysyx.org>
2025-04-08 18:06:20 +0800

    > sim RTL ysyx_24090014 董泰恒 Linux dongtaiheng-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:06:20 up 17:02,  1 user,  load average: 1.76, 1.32, 1.24

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -71,11 +71,12 @@ void bootloader() {
 
    
 
-    asm volatile (
-        "mv t0, %0\n\t"
-        "jalr zero, t0, 0"
-        : : "r"(_execute_main_sram_addr) : "t0"
-    );
+asm volatile (
+    "lui t0, %%hi(_execute_main_sram_addr)\n\t"
+    "lw t0, %%lo(_execute_main_sram_addr)(t0)\n\t"
+    "jalr zero, t0, 0"
+    : : : "t0"
+);
 }
 
 
