75a037439d0c5f32b5c1de6fe6567ca52ad4d56e
tracer-ysyx <tracer@ysyx.org>
2025-05-29 23:20:22 +0800

    > sim RTL ysyx_24090014 董泰恒 Linux dongtaiheng-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:20:22 up 2 days, 11:35,  1 user,  load average: 3.57, 2.76, 2.41

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -655,7 +655,7 @@ void exec_once(NpcState *s) {
 
         s->top->clock = 0;
         s->top->eval();
-        if (tfp) tfp->dump(main_time++);
+        //if (tfp) tfp->dump(main_time++);
         // if (record_wave && tfp) tfp->dump(main_time++);
         
         s->top->eval();
@@ -667,7 +667,7 @@ void exec_once(NpcState *s) {
         // 时钟上升沿
         s->top->clock = 1;
         s->top->eval();
-        if (tfp) tfp->dump(main_time++);
+        //if (tfp) tfp->dump(main_time++);
         // if (record_wave && tfp) tfp->dump(main_time++);
         
         s->top->eval();
