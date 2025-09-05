7a3963647eeac38de68c0a44614697dd171debd8
tracer-ysyx <tracer@ysyx.org>
2025-09-05 18:34:07 +0800

    > sim RTL ysyx_24090014 董泰恒 Linux dongtaiheng-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:34:07 up 1 day, 8 min,  1 user,  load average: 2.49, 1.65, 0.86

--- abstract-machine/am/src/riscv/ysyxsoc/input.c
+++ abstract-machine/am/src/riscv/ysyxsoc/input.c
@@ -3,15 +3,15 @@
 void __am_uart_rx(AM_UART_RX_T *rx);
 
 
-void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
+/*void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   kbd->keydown = 0;
   kbd->keycode = AM_KEY_NONE;
-}
+}*/
 
-/*void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
+void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   AM_UART_RX_T rx;
   __am_uart_rx(&rx); // 调用底层UART驱动获取数据
   kbd->keydown = (rx.data != AM_KEY_NONE);
   kbd->keycode = rx.data;
-}*/
+}
 //ysyxsoc和npc暂不实现键盘输入
